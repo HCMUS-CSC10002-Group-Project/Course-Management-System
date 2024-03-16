06b7ad7a37fa3a85588be40ae33e0a510c8e439e
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-03-16 22:13:44 +0700

    CMS-15 feat: added common include files

---
+++ include/CommonEntities.h
@@ -0,0 +1,13 @@
+// entities/CommonEntities.h
+#ifndef COMMON_ENTITIES_H
+#define COMMON_ENTITIES_H
+
+#include "../src/entities/AcademicStaff.h"
+#include "../src/entities/Class.h"
+#include "../src/entities/Course.h"
+#include "../src/entities/SchoolYear.h"
+#include "../src/entities/Semester.h"
+#include "../src/entities/Student.h"
+#include "../src/entities/User.h"
+
+#endif // COMMON_ENTITIES_H
---
+++ include/CommonInclude.h
@@ -0,0 +1,11 @@
+#ifndef COMMON_INCLUDE_H
+#define COMMON_INCLUDE_H
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+// add more std libraries here
+
+#endif // COMMON_INCLUDE_H
