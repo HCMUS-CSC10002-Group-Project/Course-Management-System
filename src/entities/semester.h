7adbd9b550a3c200b0ba3b8311ce88e3c55873d7
ng0x <161100173+ng0x@users.noreply.github.com>
2024-03-18 15:05:18 +0700

    CMS-39 feat: remove some getters and setters

--- src/entities/semester.h
+++ src/entities/semester.h
@@ -9,20 +9,15 @@ class Semester
     private:
         string semester_ID;
         string year_ID;
-        int semester_number;
         Date start_date;
         Date end_date;
     public:
         //getter
         string getSemesterID();
         string getYearID();
-        int getSemesterNumber();
         Date getStartDate();
         Date getEndDate();
         //setter
-        void setSemesteID(string newSemesterID);
-        void setYearID(string newYearID);
-        void setSemesterNumber(string newSemesterNumber);
         void setStartDate(Date newStartDate);
         void setEndDate(Date newEndDate);
 };
