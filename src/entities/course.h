f0bc85d6a514e46ad6036667aeba430e16313cd4
ebylxn <buitheanh1003@gmail.com>
2024-03-18 19:56:48 +0700

    CMS-59-Course feat: added getter/setter prototypes

--- src/entities/course.h
+++ src/entities/course.h
@@ -3,11 +3,12 @@
 
 #include <string>
 #include "student.h"
+#include "scoreboard.h"
 #include "../data_structures/LinkedList.h"
 
 using std::string;
 
-//TODO: add scoreboard.h and add getters/setters
+//TODO: add getters/setters
 
 class Course
 {
@@ -16,13 +17,31 @@ class Course
         string course_ID;
         string semester_ID;
         string class_ID;
-        LinkedList <string> teacher_name;
+        LinkedList <string> teacher_names;
         int number_of_credits;
         int capacity;
         string day_of_week;
         string session_time;
         LinkedList <Student> enrolled_students;
         //Scoreboard score_board;
+
+    public:
+        //getters and setters
+        string getCourseID();
+        string getSemesterID();
+        bool setSemesterID(string newSemesterID);
+        string getClassID();
+        LinkedList <string> getTeacherNames();
+        int getNUmberOfCredits();
+        int getCapacity();
+        bool setCapacity(int newCapacity);
+        string getDayOfWeek();
+        bool setDayOfWeek(string newDayOfWeek);
+        string getSessionTime();
+        bool setSessionTime(string newSessionTime);
+        LinkedList <Student> getEnrolledStudentsList();
+        bool addStudent(string newStudentID);
+        bool removeStudent(string studentID);
 };
 
 #endif //COURSE_H
