e1c46930dc2a7f1955bf9ad494f64a67a544d279
ebylxn <buitheanh1003@gmail.com>
2024-03-19 20:47:42 +0700

    CMS-63-ScoreboardService feat: added ScoreboardService

--- src/services/scoreboardService.h
+++ src/services/scoreboardService.h
@@ -2,5 +2,16 @@
 #define SCOREBOARD_SERVICE_H
 
 #include "../entities/scoreboard.h"
+#include "../entities/student.h"
+#include "../data_structures/LinkedList.h"
+
+class scoreboardService
+{
+    public:
+        bool setScore(map<Student,DetailedScore> scoreSheet, float score, string scoreType);
+        // scoreType is to know whether the score is for midterms, finals, etc.)
+        Scoreboard viewCourseScoreboard(string courseID);
+        LinkedList<Scoreboard> viewClassScoreboard(string classID);
+};
 
 #endif SCOREBOARD_SERVICE_H
