651d864590f46627185a79299986ea3dc6dd7b2e
ng0x <161100173+ng0x@users.noreply.github.com>
2024-03-19 09:06:46 +0700

    CMS-83 feat: add getters and setters

--- src/entities/date.h
+++ src/entities/date.h
@@ -7,6 +7,14 @@ class Date
         int month;
         int date;
     public:
+    //getters
+    int getDate();
+    int getMonth();
+    int getYear();
+    //setters
+    void setYear(int newDate);
+    void setMonth(int newMonth);
+    void setDate(int newDate);
     //constructor
     Date(int year, int month, int date);
 };
