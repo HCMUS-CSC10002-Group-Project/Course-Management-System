e8bb78691035abed82f7e5b666872f48dab708f1
ng0x <161100173+ng0x@users.noreply.github.com>
2024-03-19 19:35:09 +0700

    CMS-62 feat: fix the "include errors"

--- src/services/courseService.h
+++ src/services/courseService.h
@@ -1,10 +1,17 @@
 #ifndef COURSESERVICE_H
 #define COURSESERVE_H
-#include "course.h"
-#include "student.h"
+#include "../entities/course.h"
+#include "../entities/student.h"
+#include "../data_structures/LinkedList.h"
 class CourseService
 {
     public: 
     bool addCourse(Course course);
+    bool updateCourse(Course course);
+    bool enrollStudentInCourse(Student student);
+    bool unenrollStudentFromCourse(Student student);
+    bool deleteCourse(Course course);
+    LinkedList<Course> viewCourse(LinkedList<Course> course);
+    LinkedList<Student> viewStudentInCourse(LinkedList<Student> student);
 };
 #endif
