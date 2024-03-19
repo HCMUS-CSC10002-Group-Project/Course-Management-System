1b10b967f00db06f1d5476852bb1ebd01db6eb5d
btanh3812155 <148515621+btanh3812155@users.noreply.github.com>
2024-03-19 15:26:49 +0700

    CMS-36-AcademicStaff feat: added AcademicStaff class (#32)
    * CMS-36-AcademicStaff feat: added AcademicStaff class
    
    * CMS-36-AcademicStaff feat: changed ID setter return type to bool
    
    * CMS-36-AcademicStaff feat: removed setStaffID
    
    * CMS-36-AcademicStaff feat: added constructor
    
    ---------
    
    Co-authored-by: Chloe Bui <79560670+SparkleZ4296@users.noreply.github.com>

---
+++ src/entities/academicStaff.h
@@ -0,0 +1,15 @@
+#ifndef ACADEMIC_STAFF_H
+#define ACADEMIC_STAFF_H
+
+#include "user.h"
+
+class AcademicStaff : public User
+{
+    private:
+        string staff_ID;
+    public:
+        AcademicStaff();
+        string getStaffID();
+};
+
+#endif ACADEMIC_STAFF_H
