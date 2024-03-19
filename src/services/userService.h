444fba136f1a487d4f47f7f0b43ef3868e2f9b21
ebylxn <buitheanh1003@gmail.com>
2024-03-19 19:50:44 +0700

    CMS-100-UserService feat: created UserService class

--- src/services/userService.h
+++ src/services/userService.h
@@ -3,13 +3,16 @@
 
 #include "../entities/user.h"
 
-bool authenticateUser(string passwordHash, string username);
-bool login(string password, string username);
-bool logout();
-bool registerUser(string username, string password, string fullName, string role, string department, string email);
-bool changePassword(string newPassword);
-void viewProfile();
-bool updateUserProfile(string newFullName, string newEmail, string newRole, string newDepartment);
-string hashPassword(string password);
+class UserService{
+    public:
+        bool authenticateUser(string passwordHash, string username);
+        bool login(string password, string username);
+        bool logout();
+        bool registerUser(string username, string password, string fullName, string role, string department, string email);
+        bool changePassword(string newPassword);
+        void viewProfile();
+        virtual bool updateUserProfile(string newFullName, string newEmail, string newRole, string newDepartment);
+        string hashPassword(string password);
+};
 
 #endif //USER_SERVICE_H
