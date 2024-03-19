c2077cb44f965d706d1b8c0ca6257cd7dedd33c5
ebylxn <buitheanh1003@gmail.com>
2024-03-19 21:11:19 +0700

    CMS-63-ScoreboardService feat: changed scoreSheet variable in setScore to pass-by-reference

--- src/services/scoreboardService.h
+++ src/services/scoreboardService.h
@@ -8,7 +8,7 @@
 class ScoreboardService
 {
     public:
-        bool setScore(map<Student,DetailedScore> scoreSheet, float score, string scoreType);
+        bool setScore(map<Student,DetailedScore> &scoreSheet, float score, string scoreType);
         // scoreType is to know whether the score is for midterms, finals, etc.)
         Scoreboard viewCourseScoreboard(string courseID);
         LinkedList<Scoreboard> viewClassScoreboard(string classID);
