5a710d776249b0c26e7076f341a2318e7a4f0070
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-03-22 22:00:42 +0700

    CMS-60 feat: change setName to bool

--- src/entities/class.h
+++ src/entities/class.h
@@ -4,20 +4,21 @@
 #include "../data_structures/LinkedList.h"
 class Class
 {
-    private: 
-        string class_ID;
-        string name;
-        LinkedList<Student> students;
-    public: 
-        //getters
-        string getClassID();
-        string getName();
-        LinkedList<Student> getListOfStudents();
-        //setters
-        void setName(string name);
-        bool addStudent(Student student);
-        bool removeStudent(Student student);
-        //constructor
-        Class(string name, LinkedList<Student> students);
+private:
+    string class_ID;
+    string name;
+    LinkedList<Student> students;
+
+public:
+    // getters
+    string getClassID();
+    string getName();
+    LinkedList<Student> getListOfStudents();
+    // setters
+    bool setName(string name);
+    bool addStudent(Student student);
+    bool removeStudent(Student student);
+    // constructor
+    Class(string name, LinkedList<Student> students);
 };
 #endif
