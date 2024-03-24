8d25d5c95515afae85a9b1cbe5c371c7cd96790c
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-03-25 06:50:35 +0700

    CMS-95 feat: added delete user method

--- src/services/userService.h
+++ src/services/userService.h
@@ -3,16 +3,18 @@
 
 #include "../entities/user.h"
 
-class UserService{
-    public:
-        bool authenticateUser(string passwordHash, string username);
-        bool login(string password, string username);
-        bool logout();
-        bool registerUser(string username, string password, string fullName, string role, string department, string email);
-        bool changePassword(string newPassword);
-        void viewProfile();
-        virtual bool updateUserProfile(string newFullName, string newEmail, string newRole, string newDepartment);
-        string hashPassword(string password);
+class UserService
+{
+public:
+    bool authenticateUser(string passwordHash, string username);
+    bool login(string password, string username);
+    bool logout();
+    bool registerUser(string username, string password, string fullName, string role, string department, string email);
+    bool changePassword(string newPassword);
+    void viewProfile();
+    bool deleteUser(string username);
+    virtual bool updateUserProfile(string newFullName, string newEmail, string newRole, string newDepartment);
+    string hashPassword(string password);
 };
 
-#endif //USER_SERVICE_H
+#endif // USER_SERVICE_H
