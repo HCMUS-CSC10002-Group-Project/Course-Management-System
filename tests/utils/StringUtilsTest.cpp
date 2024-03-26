6ba9426aa1bdcbfe6eeb9671e8ec7521b4a0ce04
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-03-26 16:18:26 +0700

    CMS-86 feat: added test cases for string split and join

---
+++ tests/utils/StringUtilsTest.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+#include "StringUtils.h"
+
+TEST(StringUtilsTest, Split)
+{
+    std::string str = "Hello,World,!";
+    DynamicArray<std::string> parts = split(str, ',');
+    ASSERT_EQ(parts.Size(), 3);
+    ASSERT_EQ(parts.Get(0), "Hello");
+    ASSERT_EQ(parts.Get(1), "World");
+    ASSERT_EQ(parts.Get(2), "!");
+}
+
+TEST(StringUtilsTest, Join)
+{
+    DynamicArray<std::string> parts;
+    parts.Add("Hello");
+    parts.Add("World");
+    parts.Add("!");
+    std::string joined = join(parts, ',');
+    ASSERT_EQ(joined, "Hello,World,!");
+}
+
+TEST(StringUtilsTest, SplitMultipleDelimiter)
+{
+    std::string str = "Hello,World,1;2;3";
+    DynamicArray<std::string> parts = split(str, ',');
+    ASSERT_EQ(parts.Size(), 3);
+    ASSERT_EQ(parts.Get(0), "Hello");
+    ASSERT_EQ(parts.Get(1), "World");
+    ASSERT_EQ(parts.Get(2), "1;2;3");
+    DynamicArray<std::string> parts2 = split(parts.Get(2), ';');
+    ASSERT_EQ(parts2.Size(), 3);
+    ASSERT_EQ(parts2.Get(0), "1");
+    ASSERT_EQ(parts2.Get(1), "2");
+    ASSERT_EQ(parts2.Get(2), "3");
+}
+
+TEST(StringUtilsTest, JoinMultipleDelimiter)
+{
+    DynamicArray<std::string> parts;
+    parts.Add("Hello");
+    parts.Add("World");
+    parts.Add("1;2;3");
+    std::string joined = join(parts, ',');
+    ASSERT_EQ(joined, "Hello,World,1;2;3");
+}
