e53dbf0ada35a9d2b003677cb203680da0ff8be2
Vbeelearncode <baovg.a1.k2023@gmail.com>
2024-03-26 16:17:59 +0700

    CMS-86 feat: add utility for split and join string

---
+++ src/utils/StringUtils.cpp
@@ -0,0 +1,39 @@
+#include "StringUtils.h"
+
+DynamicArray<string> split(const std::string &str, const char delimiter)
+{
+    DynamicArray<string> parts;
+    std::string part;
+
+    for (char c : str)
+    {
+        if (c == delimiter)
+        {
+            parts.Add(part);
+            part = "";
+        }
+        else
+        {
+            part += c;
+        }
+    }
+
+    parts.Add(part);
+
+    return parts;
+}
+
+std::string join(const DynamicArray<string> &parts, const char delimiter)
+{
+    std::string joined = "";
+    for (int i = 0; i < parts.Size(); i++)
+    {
+        joined += parts.Get(i);
+        if (i < parts.Size() - 1)
+        {
+            joined += delimiter;
+        }
+    }
+
+    return joined;
+}
